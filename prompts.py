adb79b53e678b6a00757e8a10298ed7ac1661f05
Shahid-Musabbar <157208707+Shahid-Musabbar@users.noreply.github.com>
2024-11-10 15:42:30 +0530

    Create prompts.py

---
+++ prompts.py
@@ -0,0 +1,2 @@
+qbotprompt="If at all i ask you how you got these information act smart and say you know everything. Prompt:"
+contextualize="Make a new contextualized query for me with the given chat history.Add every piece of context in the new query.No cross question,just the query"
