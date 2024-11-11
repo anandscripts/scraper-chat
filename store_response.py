e55e82c6e3ce14ea63b0c397843730fcd74b06b5
Anand <83623438+anandscripts@users.noreply.github.com>
2024-11-11 12:10:53 +0530

    Update store_response.py

--- store_response.py
+++ store_response.py
@@ -1,17 +1,15 @@
-from chromadb import chromadb
-from chromadb.config import Settings
-from google.oauth2 import service_account
-import google.generativeai as genai
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-from pymongo import MongoClient
 import uuid
-from datetime import datetime, timezone
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
 from langchain_openai import ChatOpenAI
-import os
+from pymongo import MongoClient
+from datetime import datetime, timezone
 import dotenv
+import os
 import prompts
-# import text
+
+CHROMA_PATH = "store_path"
 
 dotenv.load_dotenv()
 
@@ -19,45 +17,22 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 MONGO_URI = os.getenv("MONGO_URI")
 
-genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
-
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 llm = ChatOpenAI(model='gpt-4o-mini')
 
-# credentials = service_account.Credentials.from_service_account_file("C:/Users/Gopi/Desktop/chatbot/scrape_api/sapient-flare-414821-67e257076a0d.json")
-# embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", credentials=credentials)
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
-# client = chromadb.Client(Settings())  # Using default settings for local storage
-client = chromadb.PersistentClient(path="chromadb/")
 
 mongoclient = MongoClient(MONGO_URI)
 db = mongoclient.chatbot
 
-
-def chunk_text(text, chunk_size=200):
-    words = text.split()
-    return [" ".join(words[i:i + chunk_size]) for i in range(0, len(words), chunk_size)]
-
-def get_embedding(text):
-    vector = embeddings.embed_query(text)
-    return vector
-
-def generate_unique_id(index):
-    return f"chunk_{id}_{index}"
-
-
 def store_text(text):
     website_id = str(uuid.uuid4())
-    # collection = client.get_collection(name=id)
-    collection = client.create_collection(name= website_id)
-    chunks = chunk_text(text, chunk_size=250)
-    for idx, chunk in enumerate(chunks):
-        vector = get_embedding(chunk)  
-        unique_id = generate_unique_id(idx)  
-        collection.add(documents=[chunk], embeddings=[vector], metadatas=[{"text": chunk}], ids=[unique_id])
-        return website_id
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=20)
+    text_chunks = text_splitter.split_documents(text)
+
+    Chroma.from_documents(text_chunks, embeddings, persist_directory=CHROMA_PATH, collection_name=website_id)
+    return website_id
 
 def store_chat_history(question, answer, userid, chatbotid):
     chat_data = {
@@ -71,20 +46,24 @@ def store_chat_history(question, answer, userid, chatbotid):
     }
     db.chats.insert_one(chat_data)
 
-def query_bot(prompt,id):
-    prompt_vector = get_embedding(prompt)  
+
+def query_bot(query, id):
     try:
-        collection = client.get_collection(name=id)
-        # collection.peek()
-        n_results=1
-        results = collection.query(query_embeddings=[prompt_vector], n_results=n_results)
-        similar_text =""
-        #similar_text = results['documents'][0][0]
-        for i in range(n_results):
-            similar_text=similar_text+results['documents'][0][i]
-            # print(similar_text)
-        response = llm.invoke(similar_text+prompts.qbotprompt+prompt)
+        db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embeddings, collection_name=id)
+
+        # embedding = embeddings.embed_query(query)
+        # docs = db.similarity_search_by_vector(embedding, k=3)  
+
+        retriever = db.as_retriever(
+            search_type="similarity_score_threshold", search_kwargs={"score_threshold": 0.1}
+        )
+        docs = retriever.invoke(query)
+
+        page_contents = "\n\n".join([doc.page_content for doc in docs])
+        print(page_contents)
+        response = llm.invoke(f'Website Content:\n"{page_contents}"\n'+prompts.qbotprompt_new+query)
         return response.content
+
     except KeyError as e:
         return f"Error: Collection '{id}' not found in Chroma DB. {e}"
     
@@ -99,14 +78,15 @@ def chat_history(userid, chatbotid):
         return history_list
     return None
     
-def properresponse(question, userid, chatbotid):
+def proper_query(question, userid, chatbotid):
     history_list = chat_history(userid, chatbotid)
     if history_list:
         history_text = "\n".join([f"User: {entry['data']['user']}\nBot: {entry['data']['bot']}" for entry in history_list])
-        response = llm.invoke(prompts.contextualize+f"Chat History:\n{history_text}\nQuery: {question}")
+        response = llm.invoke(f"Chat History:\n{history_text}\nUser Query: {question}\n"+prompts.contextualize_new)
         response = response.content
     else:
         response = question
+    print(response)
     result = query_bot(response, chatbotid)
     store_chat_history(question, result, userid, chatbotid)
     return result
@@ -118,13 +98,3 @@ def notification(userid, chatbotid):
         response = llm.invoke(f"Chat History:\n{history_text}\n\nBased on this chat history, provide a helpful message to encourage the user to continue chatting. Respond only with the message.")
         return {"data": history_list, "response": response.content}
     return {"data": None}
-
-
-# print(store_text(text.text))
-# query_bot('What is the company name?', chatbotid)
-
-question="Who is the technical support of the company"
-userid = 'user123'
-chatbotid= '17387dfb-5307-4ea3-8e92-09f6518991aa'
-
-# print(properresponse(question, userid, chatbotid))
